549448dbace524ebdd976fb5e092a535d3fd2426
kjyothimurali <kjyothimurali1105@gmail.com>
2026-01-09 23:24:08 +0530

    Update app_linear_regression.py

--- Simple_Linear_Regression/app_linear_regression.py
+++ Simple_Linear_Regression/app_linear_regression.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import seaborn as sns
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -9,9 +10,16 @@ from sklearn.metrics import mean_absolute_error,mean_squared_error, r2_score
 #page config #
 st.set_page_config("Linear Regression App ",layout="centered")
 # Load css #
-def load_css(file):
-    with open(file) as f:
-        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+def load_css(filename):
+    base_dir = os.path.dirname(__file__)   # folder of app_linear_regression.py
+    css_path = os.path.join(base_dir, filename)
+
+    if os.path.exists(css_path):
+        with open(css_path) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    else:
+        st.warning("style.css not found. Using default Streamlit style.")
+
 load_css("style.css")
 # Title and description #
 st.markdown("""
@@ -21,17 +29,11 @@ st.markdown("""
 
             </div>
             """,unsafe_allow_html=True)
-import os
-
-def load_css(file):
-    if os.path.exists(file):
-        with open(file) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    else:
-        st.warning("style.css not found. Using default Streamlit style.")
-
-load_css("style.css")
-
+# load data #
+@st.cache_data
+def load_data():
+    return sns.load_dataset("tips")
+df=load_data()
 
 # Data Preview #
 st.markdown('<div class="card"><h3><b>Data Preview</b></h2>',unsafe_allow_html=True)
@@ -96,5 +98,4 @@ st.markdown('<div class = "card"><h3><b>Predict tip Amount</b></h3>',unsafe_allo
 bill=st.slider("Total Bill Amount (Rs.)",float(df['total_bill'].min()),float(df['total_bill'].max()),30.0)
 tip=model.predict(scaler.transform([[bill]]))[0]
 st.markdown(f'<div class="prediction-box">Predicted Tip: (RS.) {tip:.2f}</div>',unsafe_allow_html=True)
-
 st.markdown('</div>',unsafe_allow_html=True)
